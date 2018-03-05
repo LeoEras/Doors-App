245f55666ea03b471c048954fb0fbaf048c646b9
LeoEras <leoded94@hotmail.com>
2018-03-05 16:29:21 -0500

    Agregada librería para camara

--- app/src/main/java/ec/edu/espol/cti/doors/MainActivity.java
+++ app/src/main/java/ec/edu/espol/cti/doors/MainActivity.java
@@ -1,13 +1,49 @@
 package ec.edu.espol.cti.doors;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Scanner
+        mButton = (Button) findViewById(R.id.qrscannerbtn);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+                integrator.initiateScan();
+            }
+        });
+
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        tv = (TextView) findViewById(R.id.text1);
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanResult != null) {
+            String re = scanResult.getContents();
+            tv.setText(re);
+        } else {
+            tv.setText("Operacion cancelada por el usuario");
+        }
     }
 }
